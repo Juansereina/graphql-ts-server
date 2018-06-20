2708f1f2e2a2cfe53a8cb44c09d861d41ab3693c
Juansereina <juan-2296@hotmail.es>
2018-06-20 14:31:07 -0500

    Setting up a Test Database with TypeORM

--- src/test/register.test.ts
+++ src/test/register.test.ts
@@ -1,7 +1,29 @@
-function sum(a: number, b: number) {
-  return a + b;
-}
+import { request } from "graphql-request";
+import { host } from "./constansts";
+import { createTypeormConnection } from "../utils/createTypeormConnections";
+import { User } from "../entity/User";
 
-test('adds 1 + 2 to equal 3', ()=> {
-    expect(sum(1,2)).toBe(3);
-})
+const email = "juan@reina.com";
+const password = "lokjashdfasdf";
+
+const mutation = `
+mutation{
+  register(email: "${email}", password: "${password}")
+}
+`;
+
+beforeAll(async () => {
+  await createTypeormConnection();
+});
+
+test("Register user", async () => {
+  const response = await request(host, mutation);
+  expect(response).toEqual({ register: true });
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+  const user = users[0];
+  expect(user.email).toEqual(email);
+  expect(user.password).not.toEqual(password);
+});
+
+afterAll(() => setTimeout(() => process.exit(), 500));
